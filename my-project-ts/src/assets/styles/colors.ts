90b79ba2b1f315e670b449953e82356db54e1ed3
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-10-06 21:52:30 -0300

    add new colors

--- my-project-ts/src/assets/styles/colors.ts
+++ my-project-ts/src/assets/styles/colors.ts
@@ -9,8 +9,10 @@ export const colors = {
     shadowColor2: "#00000080",
 
     borderLine: "#d6d696",
+    borderLine2: "#a9c083",
 
     textColor1: "black",
     textColor2: "white",
+    textColor3: "#4d4d4d", 
     textColorHover: "darkCyan",
 }
