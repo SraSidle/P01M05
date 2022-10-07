04b2df51b5652a251c7fe2517952b4cabc2a1f50
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-10-06 21:52:17 -0300

    add booststrap icons

--- my-project-ts/src/components/GameItem/index.tsx
+++ my-project-ts/src/components/GameItem/index.tsx
@@ -1,19 +1,38 @@
 import * as S from "./style";
 import {ReactComponent as Favorites} from "assets/icons/heart.svg";
 import { GamesResponse } from "types/Games";
+import FormEdit from "components/FormEdit";
+import { useState } from "react";
 
 type GameItemProps = {
   game: GamesResponse;
+  key: string;
 }
 
-function GameItem({game} : GameItemProps) {
+function GameItem({game, key} : GameItemProps) {
+
+  const [editGameModal, setEditGameModal] = useState(false);
+
+  const [deleteGameModal, setDeleteGameModal] = useState(false);
+
   return (
-    <S.Card>
+    <S.Card className="" key={key}>
      <S.GameName>{game.name}</S.GameName>
        <S.FavoriteGame><Favorites/></S.FavoriteGame>
          <S.Genre>{game.genre}</S.Genre>
        <S.imageGame src={game.image} alt={`imagem do jogo: ${game.name}`}/>
      <S.GameDescription>{game.description}</S.GameDescription>
+       {editGameModal && 
+        <FormEdit
+          gameId={game.id}
+          gameName={game.name}
+        />}
+        <button onClick={() => setEditGameModal(true)}>
+         <i className="bi bi-pencil"></i>
+        </button>
+        <button onClick={() => setDeleteGameModal(true)}>
+         <i className="bi bi-trash"></i>
+        </button>
     </S.Card>
   );
 }
