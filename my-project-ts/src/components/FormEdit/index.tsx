a2a38660fc00240970e1ba365a370411441b597a
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-10-07 22:10:36 -0300

    form chama o update, mas n pega o valor dos inputs

--- my-project-ts/src/components/FormEdit/index.tsx
+++ my-project-ts/src/components/FormEdit/index.tsx
@@ -1,77 +1,99 @@
 import * as S from "./style";
 import { useGames } from "hooks/useGame";
 import { useState } from "react";
+import OverlayModal from "components/OverlayModal";
 
-function FormEdit(gameId: any, gameName: string) {
+type FormEditProps = {
+  gameId: any;
+  gameName: string;
+  gameDescription: string;
+  gameGenre: string;
+  gameImg: string;
+  closeModal: any;
+};
+
+function FormEdit({
+  gameId,
+  gameName,
+  gameDescription,
+  gameGenre,
+  gameImg,
+  closeModal,
+}: FormEditProps) {
   const { getAllGames, updateGame } = useGames();
 
-  const [ editedGame, setEditedGame ] = useState({
+  const [editedGame, setEditedGame] = useState({
     id: gameId,
     newName: gameName,
-    newDescription: "",
-    newGenre: "",
-    newImage: "",
+    newDescription: gameDescription,
+    newGenre: gameGenre,
+    newImage: gameImg,
   });
 
   const editGame = async (game: any, gameToUpdate: any) => {
     delete gameToUpdate.id;
     await updateGame(game, gameToUpdate);
-    //closeModal();
+    closeModal();
   };
 
-  return(
-    <>
-    <form   
-        onSubmit={(event) => {
-        event.preventDefault();
-        editGame(editedGame.id, editedGame
-          // {
-          //   name: editedGame.newName,
-          //   description: editedGame.newDescription,
-          //   genre: editedGame.newGenre,
-          //   image: editedGame.newImage
-          //  })
-        )
-         }
-        }
+  return (
+    <OverlayModal closeModal={closeModal}>
+      <S.EditBody>
+        <p>Edite o Jogo</p>
+        <S.FormEdit
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(editedGame);
+            editGame(editedGame.id, editedGame);
+          }}
         >
-        <label>Nome do jogo</label>
-        <input
-          id="name_game"
-          value={editedGame.newName}
-          onChange={(e) => setEditedGame({...editedGame , newName: e.target.value})}
-          type="text"
-          placeholder="Super Mario"
-          required/>
-        <label>Prévia</label>
-        <input
-          id="description_game"
-          value={editedGame.newDescription}
-          onChange={(e) => setEditedGame({...editedGame , newDescription: e.target.value})}
-          type="text" 
-          placeholder={""}
-          required
-        /> 
-        <label>Gênero/Classificação</label>
-        <input
-          id="genre_game"
-          value={editedGame.newGenre}
-          onChange={(e) => setEditedGame({...editedGame , newGenre: e.target.value})}
-          type="text"
-          placeholder=""
-          required
-        />
-        <label>Foto</label>
-        <input
-          id="image_game"
-          value={editedGame.newImage}
-          onChange={(e) => setEditedGame({...editedGame , newImage: e.target.value})}
-          type="url"
-          required
-        />
-        <button type="submit">Editar</button>
-      </form>
-    </>
+          <label>Nome do jogo</label>
+          <input
+            id="name_game"
+            value={editedGame.newName}
+            onChange={(e) =>
+              setEditedGame({ ...editedGame, newName: e.target.value })
+            }
+            type="text"
+            placeholder="Super Mario"
+            required
+          />
+          <label>Prévia</label>
+          <input
+            id="description_game"
+            value={editedGame.newDescription}
+            onChange={(e) =>
+              setEditedGame({ ...editedGame, newDescription: e.target.value })
+            }
+            type="text"
+            placeholder={""}
+            required
+          />
+          <label>Gênero/Classificação</label>
+          <input
+            id="genre_game"
+            value={editedGame.newGenre}
+            onChange={(e) =>
+              setEditedGame({ ...editedGame, newGenre: e.target.value })
+            }
+            type="text"
+            placeholder=""
+            required
+          />
+          <label>Foto</label>
+          <input
+            id="image_game"
+            value={editedGame.newImage}
+            onChange={(e) =>
+              setEditedGame({ ...editedGame, newImage: e.target.value })
+            }
+            type="url"
+            required
+          />
+          <button type="submit">Editar</button>
+        </S.FormEdit>
+      </S.EditBody>
+    </OverlayModal>
   );
 }
 
--- my-project-ts/src/components/FormEdit/style.ts
+++ my-project-ts/src/components/FormEdit/style.ts
@@ -1,8 +1,34 @@
 import styled, { css } from "styled-components";
+import theme from "assets/styles/theme";
 
-export const FormEdit = styled.div`
-  width:100%;
-  height: 100%;
-  position: fixed;
-  z-index: 9999;
+export const EditBody = styled.div`
+  text-align: center;
+
+  p{
+    color: ${theme.colors.textColorHover};
+  }
+`;
+
+export const FormEdit = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: .7rem;
+
+  input {
+    padding: .3rem;
+    border-radius: .3rem;
+    border: 1px solid ${theme.colors.borderLine2};
+    outline: 0;
+  }
+
+  button {
+    padding: .5rem;
+    background-color: ${theme.colors.color2};
+    color: ${theme.colors.textColor2};
+    border: none;
+    border-radius: .3rem;
+    margin-top: .7rem;
+  }
 `;
