e9b4b8f0ae13de085be03af407c053004d6ddbb2
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-10-07 22:09:59 -0300

    preparo para o crud(update e delete n funcional)

--- my-project-ts/src/hooks/useGame.tsx
+++ my-project-ts/src/hooks/useGame.tsx
@@ -1,33 +1,52 @@
 import { GamesResponse } from "types/Games";
-import { GameService } from "services/GameService"
-import { useCallback, useState } from "react";;
+import { GameService } from "services/GameService";
+import { useCallback, useState } from "react";
 
 export const useGames = () => {
   const [games, setGames] = useState<GamesResponse[]>([]);
 
   const getAllGames = useCallback(async () => {
-    const { status, data } = await GameService.getAllGames()
+    const { status, data } = await GameService.getAllGames();
 
-    if(status !== 200) throw new Error();
+    if (status !== 200) throw new Error();
 
     setGames(data);
-  }, [])
+  }, []);
 
-  const createGame = useCallback(async(
-    game: Pick<GamesResponse,
-    "name" 
-  & "description"
-  & "genre"
-  & "image">) => {
-    
-    const { status } = await GameService.createGame(game);
+  const createGame = useCallback(
+    async (
+      game: Pick<GamesResponse, "name" & "description" & "genre" & "image">
+    ) => {
+      const { status } = await GameService.createGame(game);
 
-    if(status !== 201) throw new Error();
-  }, [])
+      if (status !== 201) throw new Error();
+    },
+    []
+  );
+
+  const updateGame = useCallback(
+    async (
+      id: string,
+      game: Pick<GamesResponse, "name" & "description" & "genre" & "image">
+    ) => {
+      const { status } = await GameService.updateGame(id, game);
+
+      if (status !== 200) throw new Error();
+    },
+    []
+  );
+
+  const deleteGame = useCallback(async (id: string) => {
+    const { status } = await GameService.deleteGame(id);
+
+    if (status !== 202) throw new Error();
+  }, []);
 
   return {
     games,
     getAllGames,
     createGame,
-  }
-}
+    updateGame,
+    deleteGame,
+  };
+};
--- my-project-ts/src/services/GameService.ts
+++ my-project-ts/src/services/GameService.ts
@@ -3,13 +3,20 @@ import { GamesResponse } from "types/Games";
 
 const getAllGames = () => Api.get<GamesResponse[]>("/games");
 
-const createGame = (game: Pick<GamesResponse,
-    "name" 
-  & "description"
-  & "genre"
-  & "image">) => Api.post("/games", game);
+const createGame = (
+  game: Pick<GamesResponse, "name" & "description" & "genre" & "image">
+) => Api.post("/games", game);
+
+const updateGame = (
+  id: string,
+  game: Pick<GamesResponse, "name" & "description" & "genre" & "image">
+) => Api.put(`/games/${id}`, game);
+
+const deleteGame = (id: string) => Api.delete(`/games/${id}`);
 
 export const GameService = {
   getAllGames,
   createGame,
-}
+  updateGame,
+  deleteGame,
+};
